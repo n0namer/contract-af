81bbb9203df477c0de97d52c8e880102bb1974f8
Никита Гамолин <nik.gam@gmail.com>
2026-08-27 11:56:55 +0300

    test(contract): align adversary hidden-trap call signature

--- tests/test_review_layer.py
+++ tests/test_review_layer.py
@@ -390,7 +390,7 @@ class TestAdversaryReviewer:
         await _enqueue_and_sentinel(queue, [finding], ADVERSARY_SENTINEL)
 
         result = await review_as_adversary(
-            mock_app, queue, _make_intake(), _make_anatomy(), "text"
+            mock_app, queue, _make_intake(), "text"
         )
 
         assert len(result.hidden_traps) == 1
